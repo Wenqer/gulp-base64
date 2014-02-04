a256aedeeb0746c7732af19ee10c40798d145fa2
Wenqer <wenqeer@gmail.com>
2014-02-04 15:57:48 +0200

    fixes in path finding, also added options baseDir and debug mode

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ module.exports = function (opts) {
     function rebase(file, encoding, callback) {
         var self = this;
 
-        if (file.path.indexOf('src') === -1) {
-            self.push(file);
-            return callback();
-        }
-
         encode.stylesheet(file, opts, function (err, src) {
             if (err) {
                 console.error(err);
